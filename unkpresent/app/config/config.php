5d421e9dcd618958d41bf3cf2b12a8ab1f786622
OvelTjia05 <OvelTjia05@gmail.com>
2024-03-19 06:51:39 +0000

    Add Exercise unkpresent file

---
+++ unkpresent/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+	// define path constants
+    define("APP_PATH", "http://103.59.95.45/unkpresent/public");
+
+    // define database config
+    define("HOST_DB", "localhost");
+    define("NAME_DB", "db_attendance");
+    define("USER_DB", "root");
+    define("PASS_DB", "100113Ov-");
+	
+	
+?>
